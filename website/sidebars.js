0a106aee8bd7ee2818e26a86518351f52cf7e5b7
ipadorusa <ipadorusa@gmail.com>
2022-11-10 14:05:40 +0900

    quiz

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,5 +1,5 @@
 module.exports = {
   docs: {
-    "웹 접근성 퀴즈": ["quiz/quiz-1", "quiz/quiz-10", "quiz/quiz-11", "quiz/quiz-12", "quiz/quiz-13", "quiz/quiz-14", "quiz/quiz-15", "quiz/quiz-16"]
+    "웹 접근성 퀴즈": ["quiz/quiz-1", "quiz/quiz-10", "quiz/quiz-11", "quiz/quiz-12", "quiz/quiz-13", "quiz/quiz-14", "quiz/quiz-15", "quiz/quiz-16", "quiz/quiz-17"]
   },
 };
