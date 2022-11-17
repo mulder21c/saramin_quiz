7ec78bae3880f321f4feea005a30d508002eb654
mulder21c <mulder21c@gmail.com>
2022-11-17 14:42:07 +0900

    feat(sidebar): grouping by category

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,20 +1,46 @@
 module.exports = {
   docs: {
     "웹 접근성 퀴즈": [
-      "quiz/quiz-1",
-      "quiz/quiz-2",
-      "quiz/quiz-3",
-      "quiz/quiz-4",
-      "quiz/quiz-5",
-      "quiz/quiz-9",
-      "quiz/quiz-10",
-      "quiz/quiz-11",
-      "quiz/quiz-12",
-      "quiz/quiz-13",
-      "quiz/quiz-14",
-      "quiz/quiz-15",
-      "quiz/quiz-16",
-      "quiz/quiz-17",
+      {
+        type: "category",
+        label: "접근성 일반",
+        items: [
+          "quiz/quiz-1",
+          "quiz/quiz-2",
+          "quiz/quiz-5",
+          "quiz/quiz-6",
+          "quiz/quiz-9",
+        ],
+      },
+      {
+        type: "category",
+        label: "장애 이해",
+        items: ["quiz/quiz-3", "quiz/quiz-4"],
+      },
+      {
+        type: "category",
+        label: "접근성 사례",
+        items: [
+          "quiz/quiz-7",
+          "quiz/quiz-10",
+          "quiz/quiz-11",
+          "quiz/quiz-12",
+          "quiz/quiz-13",
+          "quiz/quiz-14",
+          "quiz/quiz-15",
+          "quiz/quiz-16",
+          "quiz/quiz-17",
+        ],
+      },
     ],
   },
+  //   "웹 접근성 퀴즈": [
+  //     {
+  //       "접근성 일반": {
+  //         docs: [["quiz/quiz-1"]],
+  //       },
+  //     },
+
+  //   ],
+  // },
 };
