80fc1f67a9b9b8586544cf45095576765b3ba4a2
mulder21c <mulder21c@gmail.com>
2022-11-17 16:17:51 +0900

    fix(docs): 사이드바 기준으로 라우트 재설정

--- website/sidebars.js
+++ website/sidebars.js
@@ -4,24 +4,28 @@ module.exports = {
       {
         type: "category",
         label: "접근성 일반",
+        collapsed: false,
         items: [
           "quiz/quiz-1",
           "quiz/quiz-2",
+          "quiz/quiz-3",
+          "quiz/quiz-4",
           "quiz/quiz-5",
-          "quiz/quiz-6",
-          "quiz/quiz-9",
         ],
       },
       {
         type: "category",
         label: "장애 이해",
-        items: ["quiz/quiz-3", "quiz/quiz-4"],
+        collapsed: false,
+        items: ["quiz/quiz-6", "quiz/quiz-7"],
       },
       {
         type: "category",
         label: "접근성 사례",
+        collapsed: false,
         items: [
-          "quiz/quiz-7",
+          "quiz/quiz-8",
+          "quiz/quiz-9",
           "quiz/quiz-10",
           "quiz/quiz-11",
           "quiz/quiz-12",
@@ -29,18 +33,8 @@ module.exports = {
           "quiz/quiz-14",
           "quiz/quiz-15",
           "quiz/quiz-16",
-          "quiz/quiz-17",
         ],
       },
     ],
   },
-  //   "웹 접근성 퀴즈": [
-  //     {
-  //       "접근성 일반": {
-  //         docs: [["quiz/quiz-1"]],
-  //       },
-  //     },
-
-  //   ],
-  // },
 };
